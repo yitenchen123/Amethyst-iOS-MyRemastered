89820da68fb38b3a95cf00a6418e3a23ffb5164f
yitenchen123 <2916782430@qq.com>
2026-01-14 18:58:47 +0800

    更新 ModsManagerViewController.h

--- ModsManagerViewController.h
+++ ModsManagerViewController.h
@@ -1,31 +1,25 @@
 #import <UIKit/UIKit.h>
-#import "ModVersionViewController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-// Enum to manage the controller's state
-typedef NS_ENUM(NSInteger, ModsManagerMode) {
-    ModsManagerModeLocal,
-    ModsManagerModeOnline
-};
+@class ModItem; // 前向声明
 
-typedef NS_ENUM(NSInteger, ModSectionType) {
-    ModSectionTypeEnabled,
-    ModSectionTypeDisabled,
-    ModSectionTypeCount
-};
+@interface ModDetailViewController : UIViewController
 
-@interface ModsManagerViewController : UIViewController
+// 现有的属性
+@property (nonatomic, strong) NSString *modId;
+@property (nonatomic, strong) NSString *modName;
+@property (nonatomic, strong) NSString *modDescription;
+@property (nonatomic, strong) NSString *modVersion;
+@property (nonatomic, strong) NSURL *modIconUrl;
 
-@property (nonatomic, copy, nullable) NSString *profileName;
+// 新增的属性 - 用于从 ModsManagerViewController 传递数据
+@property (nonatomic, strong) ModItem *modItem;
+@property (nonatomic, assign) NSInteger currentMode;
+@property (nonatomic, strong) NSString *profileName;
 
-// Properties for online search
-@property (nonatomic, assign) ModsManagerMode currentMode;
-@property (nonatomic, strong) NSMutableArray *onlineSearchResults;
-
-// For local mods organization
-@property (nonatomic, strong) NSMutableArray<NSMutableArray<ModItem *> *> *localModsBySection;
-@property (nonatomic, strong) NSMutableArray<NSMutableArray<ModItem *> *> *filteredLocalModsBySection;
+- (instancetype)initWithModId:(NSString *)modId;
+- (void)loadModDetails;
 
 @end
 
