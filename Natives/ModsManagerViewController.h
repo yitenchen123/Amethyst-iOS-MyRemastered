a192847653a7acebf4fbb1ca0889de6b47c09a11
yitenchen123 <2916782430@qq.com>
2026-01-14 18:16:38 +0800

    Update ModsManagerViewController.h
    Signed-off-by: yitenchen123 <2916782430@qq.com>

--- Natives/ModsManagerViewController.h
+++ Natives/ModsManagerViewController.h
@@ -9,6 +9,12 @@ typedef NS_ENUM(NSInteger, ModsManagerMode) {
     ModsManagerModeOnline
 };
 
+typedef NS_ENUM(NSInteger, ModSectionType) {
+    ModSectionTypeEnabled,
+    ModSectionTypeDisabled,
+    ModSectionTypeCount
+};
+
 @interface ModsManagerViewController : UIViewController
 
 @property (nonatomic, copy, nullable) NSString *profileName;
@@ -16,6 +22,10 @@ typedef NS_ENUM(NSInteger, ModsManagerMode) {
 // Properties for online search
 @property (nonatomic, assign) ModsManagerMode currentMode;
 @property (nonatomic, strong) NSMutableArray *onlineSearchResults;
+
+// For local mods organization
+@property (nonatomic, strong) NSMutableArray<NSMutableArray<ModItem *> *> *localModsBySection;
+@property (nonatomic, strong) NSMutableArray<NSMutableArray<ModItem *> *> *filteredLocalModsBySection;
 
 @end
 
