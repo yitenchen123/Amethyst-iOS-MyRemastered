f95ea781a19c694b13d13815dd1a40cc4f92ffe8
yitenchen123 <2916782430@qq.com>
2026-02-03 13:05:27 +0800

    更新 ShaderVersionViewController.h

--- Natives/ShaderVersionViewController.h
+++ Natives/ShaderVersionViewController.h
@@ -1,22 +1,21 @@
 #import <UIKit/UIKit.h>
 #import <UIKit/UIKit.h>
 #import "ShaderItem.h"
-#import "ModVersion.h"
+#import "ShaderVersion.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @class ShaderVersionViewController;
 
 @protocol ShaderVersionViewControllerDelegate <NSObject>
-- (void)shaderVersionViewController:(ShaderVersionViewController *)viewController
-                   didSelectVersion:(ModVersion *)version;
+- (void)shaderVersionViewController:(ShaderVersionViewController *)viewController didSelectVersion:(ShaderVersion *)version;
 @end
 
 @interface ShaderVersionViewController : UIViewController
 
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, strong) ShaderItem *shaderItem;
 @property (nonatomic, weak) id<ShaderVersionViewControllerDelegate> delegate;
-@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 
 @end
 
