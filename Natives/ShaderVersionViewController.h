f8509768a16d35f1e9149b7acae836eddc0d4e44
yitenchen123 <2916782430@qq.com>
2026-02-03 12:13:30 +0800

    创建 ShaderVersionViewController.h

---
+++ Natives/ShaderVersionViewController.h
@@ -0,0 +1,21 @@
+#import <UIKit/UIKit.h>
+#import "ShaderItem.h"
+#import "ModVersion.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class ShaderVersionViewController;
+
+@protocol ShaderVersionViewControllerDelegate <NSObject>
+- (void)shaderVersionViewController:(ShaderVersionViewController *)viewController didSelectVersion:(ModVersion *)version;
+@end
+
+@interface ShaderVersionViewController : UIViewController
+
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong) ShaderItem *shaderItem;
+@property (nonatomic, weak) id<ShaderVersionViewControllerDelegate> delegate;
+
+@end
+
+NS_ASSUME_NONNULL_END
