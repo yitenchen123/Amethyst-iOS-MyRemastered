59893fd0ca8ac0c6723b4eb451091014e0a1b877
yitenchen123 <2916782430@qq.com>
2026-02-03 12:29:14 +0800

    更新 ShaderTableViewController.m

--- Natives/ShaderTableViewController.m
+++ Natives/ShaderTableViewController.m
@@ -1,11 +1,22 @@
-#import "ShaderTableViewController.h"
+//
+//  ShaderTableViewController.m
+//  AmethystMods
+//
+//  Created by Copilot on 2025-08-22.
+//  Updated: ensure 上网搜索 switch is placed directly left of refresh and visible reliably.
+//
 
-@interface ShaderTableViewController ()
+#import "ShaderTableViewController.h"
+#import "ModTableViewCell.h"
+#import "ShaderService.h"
+#import "ShaderItem.h"
+
+@interface ShaderTableViewController () <ModTableViewCellDelegate, UISearchBarDelegate>
 
 @property (nonatomic, strong) NSArray<ShaderItem *> *mods;
 @property (nonatomic, strong) NSArray<ShaderItem *> *filteredMods;
-@property (nonatomic, strong) UISwitch *onlineSearchSwitch;
 @property (nonatomic, strong) UISearchBar *searchBar;
+@property (nonatomic, strong) UISwitch *onlineSearchSwitch;
 
 @end
 
@@ -13,35 +24,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.title = @"Mods";
-    [self.tableView registerClass:[ModTableViewCell class] forCellReuseIdentifier:@"ModCell"];
-    self.tableView.rowHeight = 96;
-
+    
+    [self.tableView registerClass:[ModTableViewCell class]
+           forCellReuseIdentifier:@"ModCell"];
+    self.tableView.rowHeight = 96.0;
+    
     UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 140, 32)];
+    
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 78, 32)];
     label.text = @"上网搜索";
     label.font = [UIFont systemFontOfSize:13];
     label.textAlignment = NSTextAlignmentRight;
     label.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleRightMargin;
     [container addSubview:label];
-
+    
     self.onlineSearchSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(86, 4, 0, 0)];
     self.onlineSearchSwitch.on = [ShaderService sharedService].onlineSearchEnabled;
-    [self.onlineSearchSwitch addTarget:self action:@selector(toggleOnlineSearch:) forControlEvents:UIControlEventValueChanged];
+    [self.onlineSearchSwitch addTarget:self
+                               action:@selector(toggleOnlineSearch:)
+                     forControlEvents:UIControlEventValueChanged];
     self.onlineSearchSwitch.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin;
     [container addSubview:self.onlineSearchSwitch];
-
-    UIBarButtonItem *switchContainerItem = [[UIBarButtonItem alloc] initWithCustomView:container];
-    UIBarButtonItem *refresh = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshTapped)];
-    self.navigationItem.rightBarButtonItems = @[refresh, switchContainerItem];
-
-    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
-    self.searchBar.placeholder = @"搜索 Mod...";
+    
+    UIBarButtonItem *switchItem =
+    [[UIBarButtonItem alloc] initWithCustomView:container];
+    
+    UIBarButtonItem *refresh =
+    [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                                                  target:self
+                                                  action:@selector(refreshTapped)];
+    
+    self.navigationItem.rightBarButtonItems = @[refresh, switchItem];
+    
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 44)];
     self.searchBar.delegate = self;
+    self.searchBar.placeholder = @"搜索 Mod...";
     self.searchBar.searchBarStyle = UISearchBarStyleMinimal;
     self.tableView.tableHeaderView = self.searchBar;
-
+    
     self.filteredMods = @[];
+    
     [self refreshTapped];
 }
 
@@ -50,29 +74,37 @@
     self.onlineSearchSwitch.on = [ShaderService sharedService].onlineSearchEnabled;
 }
 
-- (void)toggleOnlineSearch:(UISwitch *)sw {
-    [ShaderService sharedService].onlineSearchEnabled = sw.isOn;
+- (void)toggleOnlineSearch:(UISwitch *)sender {
+    [ShaderService sharedService].onlineSearchEnabled = sender.isOn;
     [self refreshTapped];
 }
 
 - (void)refreshTapped {
-    [[ShaderService sharedService] scanModsForProfile:self.profileName completion:^(NSArray<ShaderItem *> *mods) {
+    [[ShaderService sharedService] scanModsForProfile:self.profileName
+                                           completion:^(NSArray<ShaderItem *> *mods) {
         self.mods = mods ?: @[];
+        
         if (self.searchBar.text.length == 0) {
             self.filteredMods = self.mods;
         } else {
             [self filterModsForSearchText:self.searchBar.text];
         }
+        
         [self.tableView reloadData];
-
-        for (ShaderItem *m in self.mods) {
-            [[ShaderService sharedService] fetchMetadataForMod:m completion:^(ShaderItem *item, NSError * _Nullable error) {
+        
+        for (ShaderItem *mod in self.mods) {
+            [[ShaderService sharedService] fetchMetadataForMod:mod
+                                                    completion:^(ShaderItem *item, NSError * _Nullable error) {
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    NSUInteger idx = [self.mods indexOfObjectPassingTest:^BOOL(ShaderItem *obj, NSUInteger idx, BOOL *stop) {
+                    NSUInteger idx =
+                    [self.mods indexOfObjectPassingTest:^BOOL(ShaderItem *obj, NSUInteger idx, BOOL *stop) {
                         return [obj.filePath isEqualToString:item.filePath];
                     }];
+                    
                     if (idx != NSNotFound) {
-                        [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:idx inSection:0]]
+                        NSIndexPath *ip =
+                        [NSIndexPath indexPathForRow:idx inSection:0];
+                        [self.tableView reloadRowsAtIndexPaths:@[ip]
                                               withRowAnimation:UITableViewRowAnimationNone];
                     }
                 });
@@ -91,38 +123,52 @@
     if (searchText.length == 0) {
         self.filteredMods = self.mods;
     } else {
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"displayName CONTAINS[cd] %@ OR modDescription CONTAINS[cd] %@", searchText, searchText];
+        NSPredicate *predicate =
+        [NSPredicate predicateWithFormat:@"displayName CONTAINS[cd] %@ OR modDescription CONTAINS[cd] %@",
+         searchText, searchText];
         self.filteredMods = [self.mods filteredArrayUsingPredicate:predicate];
     }
     [self.tableView reloadData];
 }
 
-#pragma mark - TableView
+#pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.filteredMods.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    ModTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ModCell" forIndexPath:indexPath];
-    ShaderItem *m = self.filteredMods[indexPath.row];
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    ModTableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:@"ModCell"
+                                    forIndexPath:indexPath];
+    
+    ShaderItem *mod = self.filteredMods[indexPath.row];
     cell.delegate = self;
-    [cell configureWithMod:m displayMode:ModTableViewCellDisplayModeLocal];
+    [cell configureWithMod:mod
+               displayMode:ModTableViewCellDisplayModeLocal];
     return cell;
 }
 
 #pragma mark - ModTableViewCellDelegate
 
 - (void)modCellDidTapToggle:(UITableViewCell *)cell {
-    NSIndexPath *ip = [self.tableView indexPathForCell:cell];
-    ShaderItem *m = self.filteredMods[ip.row];
-    NSError *err = nil;
-    if (![[ShaderService sharedService] toggleEnableForMod:m error:&err]) {
-        UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"错误" message:err.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
-        [ac addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
-        [self presentViewController:ac animated:YES completion:nil];
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+    ShaderItem *mod = self.filteredMods[indexPath.row];
+    
+    NSError *error = nil;
+    if (![[ShaderService sharedService] toggleEnableForMod:mod error:&error]) {
+        UIAlertController *alert =
+        [UIAlertController alertControllerWithTitle:@"错误"
+                                            message:error.localizedDescription
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确定"
+                                                  style:UIAlertActionStyleDefault
+                                                handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
     } else {
-        [self.tableView reloadRowsAtIndexPaths:@[ip] withRowAnimation:UITableViewRowAnimationAutomatic];
+        [self.tableView reloadRowsAtIndexPaths:@[indexPath]
+                              withRowAnimation:UITableViewRowAnimationAutomatic];
     }
 }
 
