f1ce4b47739f1b7af1f49c664fe5ad471eb06a24
yitenchen123 <2916782430@qq.com>
2026-02-03 12:28:11 +0800

    更新 ShaderTableViewController.h

--- Natives/ShaderTableViewController.h
+++ Natives/ShaderTableViewController.h
@@ -1,11 +1,15 @@
+//
+//  ShaderTableViewController.h
+//  AmethystMods
+//
+//  Created by Copilot on 2025-08-22.
+//
+
 #import <UIKit/UIKit.h>
-#import "ShaderItem.h"
-#import "ShaderService.h"
-#import "ModTableViewCell.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ShaderTableViewController : UITableViewController <ModTableViewCellDelegate, UISearchBarDelegate>
+@interface ShaderTableViewController : UITableViewController
 
 @property (nonatomic, copy) NSString *profileName;
 
